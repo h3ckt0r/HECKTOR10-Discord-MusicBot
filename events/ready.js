61047a26bc90c594bb617fd2cf0e6201ad73b61c
HECKTOR10 <samyakjain.7162@mams1985.com>
2021-08-05 16:04:50 +0530

    Initial commit

---
+++ events/ready.js
@@ -0,0 +1,13 @@
+module.exports = async (client) => {
+  client.Ready = true, 
+  client.user.setPresence({
+    status: "dnd",  // You can show online, idle, and dnd
+    activity: {
+        name: "https://bot.hacktormusic.tk/",  // The message shown
+        type: "PLAYING", // PLAYING, WATCHING, LISTENING, STREAMING,
+    }
+});
+    client.Manager.init(client.user.id);
+    client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
+client.RegisterSlashCommands();
+};
